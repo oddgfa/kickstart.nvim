e034076b09084c541bfad2ab3c2a73801ae0a5c4
Odd Gunnar <oddgunnarfatland@gmail.com>
2026-01-05 14:51:45 +0100

    Add neotest

--- init.lua
+++ init.lua
@@ -984,7 +984,82 @@ require('lazy').setup({
       { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
     },
   },
-
+  {
+    'nvim-neotest/neotest',
+    dependencies = {
+      'nvim-neotest/nvim-nio',
+      'nvim-lua/plenary.nvim',
+      'antoinemadec/FixCursorHold.nvim',
+      'nvim-treesitter/nvim-treesitter',
+      'codymikol/neotest-kotlin',
+    },
+    config = function()
+      require('neotest').setup {
+        adapters = {
+          require 'neotest-kotlin',
+        },
+      }
+    end,
+    keys = {
+      -- Neotest related mappings
+      {
+        '<leader>n',
+        group = '🧪 Test',
+        nowait = true,
+        remap = false,
+      },
+      {
+        '<leader>nr',
+        "<cmd>lua require('neotest').run.run()<cr>",
+        desc = 'Run nearest test',
+      },
+      {
+        '<leader>nf',
+        "<cmd>lua require('neotest').run.run(vim.fn.expand('%'))<cr>",
+        desc = 'Run current file',
+      },
+      {
+        '<leader>na',
+        "<cmd>lua require('neotest').run.run({ suite = true })<cr>",
+        desc = 'Run all tests',
+      },
+      {
+        '<leader>nd',
+        "<cmd>lua require('neotest').run.run({strategy = 'dap'})<cr>",
+        desc = 'Debug nearest test',
+      },
+      {
+        '<leader>ns',
+        "<cmd>lua require('neotest').run.stop()<cr>",
+        desc = 'Stop test',
+      },
+      {
+        '<leader>nn',
+        "<cmd>lua require('neotest').run.attach()<cr>",
+        desc = 'Attach to nearest test',
+      },
+      {
+        '<leader>no',
+        "<cmd>lua require('neotest').output.open()<cr>",
+        desc = 'Show test output',
+      },
+      {
+        '<leader>np',
+        "<cmd>lua require('neotest').output_panel.toggle()<cr>",
+        desc = 'Toggle output panel',
+      },
+      {
+        '<leader>nv',
+        "<cmd>lua require('neotest').summary.toggle()<cr>",
+        desc = 'Toggle summary',
+      },
+      {
+        '<leader>nc',
+        "<cmd>lua require('neotest').run.run({ suite = true, env = { CI = true } })<cr>",
+        desc = 'Run all tests with CI',
+      },
+    },
+  },
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.

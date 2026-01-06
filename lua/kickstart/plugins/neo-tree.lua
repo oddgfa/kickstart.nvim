812cae7b0d249752cfee0ff1250a85f84b68229a
Odd Gunnar <oddgunnarfatland@gmail.com>
2026-01-06 13:22:43 +0100

    Neotree

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -11,13 +11,13 @@ return {
   },
   lazy = false,
   keys = {
-    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+    { 'ø', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
   },
   opts = {
     filesystem = {
       window = {
         mappings = {
-          ['\\'] = 'close_window',
+          ['ø'] = 'close_window',
         },
       },
     },
